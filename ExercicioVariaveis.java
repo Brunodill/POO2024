38e19714dabd92b1b6d3d89e0a347413b0cd25d3
Bruno Dill <159143461+Brunodill@users.noreply.github.com>
2024-02-28 09:22:26 -0300

    Add files via upload

---
+++ ExercicioVariaveis.java
@@ -0,0 +1,32 @@
+public class ExercicioVariaveis {
+    public static void main(String[] args) {
+        
+        //Cria as variaveis com seus respectivos tipos
+        int inteiro = 10;
+        long longo = 1000000000l;
+        float flutuante = 5.12f;
+        double duplo = 3.14159265359;
+        char palavra = 'A';
+        boolean booleano = true;
+        byte bytes = 127;
+        short curto = 32000;
+
+        //Printar resultados na tela
+        System.out.println("inteiro: " + inteiro);
+        System.out.println("longo: " + longo);
+        System.out.println("flutuante: " + flutuante);
+        System.out.println("duplo: " + duplo);
+        System.out.println("caractere: " + palavra);
+        System.out.println("booleano: " + booleano);
+        System.out.println("bytes: " + bytes);
+        System.out.println("curto: " + curto);
+
+        //caracteres especiais
+        System.out.println("nova linha:\nBruno\nDill");
+        System.out.println("Da um tab na linha:\t\tBruno Dill");
+        System.out.println("coloca aspas duplas : \"Texto entre aspas\"");
+        System.out.println("Cria a barra invertida: C:\\diretorio\\arquivo");
+
+    }
+}
+
